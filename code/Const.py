9f7ebc924f97fb38ce42ebf6b0f137c52d7817a4
Andrey <155186882+Andrey-3500@users.noreply.github.com>
2025-02-28 15:08:57 -0300

    Inserting assets Creating the const file Creating the menu: background, sound and fonts

---
+++ code/Const.py
@@ -0,0 +1,16 @@
+
+#C
+COLOR_ORANGE = (255,110,0)
+C_WHITE = (255,255,255)
+
+#M
+MENU_OP = ( 'NEW GAME 1P',
+            'NEW GAME 2P - COOP',
+            'NEW GAME 2P - COMP',
+            'SCORE',
+            'EXIT')
+
+
+#W
+WIN_WIDTH = 576
+WIN_HEIGTH = 324
