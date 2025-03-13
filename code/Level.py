aecbd251c4f5c96143774a5a187e3aa8cfcf0802
Andrey <155186882+Andrey-3500@users.noreply.github.com>
2025-03-13 20:02:52 -0300

    Creating Players Shot Creating Enemies Shot

--- code/Level.py
+++ code/Level.py
@@ -13,7 +13,10 @@ from pygame.surface import Surface
 
 from code import Entity
 from code.Const import WIN_HEIGTH
+from code.Enemy import Enemy
 from code.EntityFactory import EntityFactory
+from code.EntityMediator import EntityMediator
+from code.Player import Player
 
 
 class Level:
@@ -44,6 +47,10 @@ class Level:
             for ent in self.entity_list:
                 self.window.blit(source=ent.surf, dest=ent.rect)
                 ent.move()
+                if isinstance(ent, (Player, Enemy)):
+                    shoot = ent.shoot()
+                    if shoot is not None:
+                        self.entity_list.append(shoot)
 
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
@@ -60,6 +67,10 @@ class Level:
             self.level_text(24, f'fps: {clock.get_fps() :.0f}', C_WHITE, (10, WIN_HEIGTH - 35))
             self.level_text(24, f'entidades: {len(self.entity_list)}', C_WHITE, (10, WIN_HEIGTH - 20))
             pygame.display.flip()
+
+            #collisions
+            EntityMediator.verify_collision(entity_list=self.entity_list)
+            EntityMediator.verify_health(entity_list=self.entity_list)
         pass
 
     def level_text(self, text_size: int, text: str, text_color: tuple, text_pos: tuple):
